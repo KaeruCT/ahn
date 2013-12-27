3da7fb15aba0855cbe81f5c835bc4c01d23ffb02
Andres V <darklink898@gmail.com>
2013-12-27 10:46:23 -0600

    fixed links to ask hn stories

--- js/controllers.js
+++ js/controllers.js
@@ -8,6 +8,30 @@ newsListApp.controller('NewsListCtrl', function ($scope, $http) {
         url: newsUrl,
         cache: true
     }).success(function (data) {
-        $scope.news = data.items;
+        var news = [],
+            url, i, item;
+
+        for (i in data.items) {
+            item = data.items[i];
+            if (item.title.match(/^Ask HN/)) {
+                url = 'https://news.ycombinator.com/item?id=' + item.id;
+            } else {
+                url = item.url;
+            }
+
+            news.push({
+                id: item.id,
+                title: item.title,
+                url: url,
+                commentCount: item.commentCount,
+                commentUrl: 'https://news.ycombinator.com/item?id=' + item.id,
+                points: item.points,
+                postedAgo: item.postedAgo,
+                postedBy: item.postedBy,
+                postedByUrl: 'https://news.ycombinator.com/user?id=' + item.postedBy,
+            });
+        }
+
+        $scope.news = news;
     });
 });
